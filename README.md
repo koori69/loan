b4d10c1315e905d585bf4ffc6f86f3420db42af6
iT2afL0rd <it2afl0rd@gmail.com>
2014-11-02 20:51:56 +0800

    modify readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-This is a project for Node.js and Express study.
+## Loan
+This is a project based on Node.js and Express for study.
+What I'll include in it is:
+ * [Bootstrap](http://getbootstrap.com/2.3.2/)
+ * MongoDB
+
+## License
+MIT
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', layout: 'layout' });
+  res.render('index', { title: 'Express'});
 });
 
 //router.get('/u/:user', function(req, res) {
